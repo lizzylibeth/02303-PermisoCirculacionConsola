c86f60a6fb0e3fe60373e68401db231fc520f134
victo <victo@192.168.1.134>
2019-10-25 17:47:47 +0200

    :car: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,56 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+
+        boolean permisoOK;
+
+        try {
+
+            //Leer Dato
+            System.out.print("Permiso de circulación ...: ");
+            permisoOK = SCN.nextBoolean();
+
+            //Mensaje
+            System.out.printf("Permiso de circulación ...: %b%n", permisoOK = 1 != 2);
+            //La expresión da true, por eso se corresponde al valor asignado. dará true. se puede generar valores true o false sin necesariamente incluyéndolos. 
+        } catch (Exception e) {
+
+            //Mensaje
+            System.out.println("ERROR: Entrada Incorrecta");
+
+        } finally {
+            //Borrar buffer
+            SCN.nextLine();
+        }//finally 
+
+    }//main
+
+}//class
